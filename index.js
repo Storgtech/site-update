1dabc2c0e9bcb2cfbc6adb0171ef1ebb96295d97
StorgTech Tecnologia e Serviços, LDA <120728584+Storgtech@users.noreply.github.com>
2024-07-19 15:36:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
 
+  const serviceID = "YOUR_SERVICE_ID";
+  const templateID = "YOUR_TEMPLETE_ID";
+
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Sua Mensagem foi enviado com sucesso")
+
+    })
+    .catch(err=>console.log(err));
+
+}
