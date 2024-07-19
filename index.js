79a8571b018ee3522da70a088f395764079450be
StorgTech Tecnologia e Serviços, LDA <120728584+Storgtech@users.noreply.github.com>
2024-07-19 16:40:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-function sendMail() {
-  var params = {
-    email: document.getElementById("email").value,
-  };
+function emailSend(){
 
-  const serviceID = "service_4zb8hpt";
-  const templateID = "template_uzhzgwh";
+	var userName = document.getElementById('name').value;
+	var phone = document.getElementById('phone').value;
+	var email = document.getElementById('email').value;
 
-    emailjs.send(serviceID, templateID, params)
-    .then(res=>{
-        document.getElementById("email").value = "";
-        console.log(res);
-        alert("Sua Mensagem foi enviado com sucesso")
-
-    })
-    .catch(err=>console.log(err));
-
+	var messageBody = "Name " + userName +
+	"<br/> Phone " + phone +
+	"<br/> Email " + email;
+	Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "host@gmail.com",
+    Password : "Password",
+    To : 'reviever@gmail.com',
+    From : "website@gmail.com",
+    Subject : "This is the subject",
+    Body : messageBody
+}).then(
+  message => {
+  	if(message=='OK'){
+  		swal("Secussful", "You clicked the button!", "success");
+  	}
+  	else{
+  		swal("Error", "You clicked the button!", "error");
+  	}
+  }
+);
 }
