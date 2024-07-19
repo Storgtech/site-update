a97d8f965d7458ecf3ed8b43dc02743b9820c64e
StorgTech Tecnologia e Serviços, LDA <120728584+Storgtech@users.noreply.github.com>
2024-07-19 15:54:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 function sendMail() {
   var params = {
-    name: document.getElementById("name").value,
     email: document.getElementById("email").value,
-    message: document.getElementById("message").value,
   };
 
   const serviceID = "YOUR_SERVICE_ID";
